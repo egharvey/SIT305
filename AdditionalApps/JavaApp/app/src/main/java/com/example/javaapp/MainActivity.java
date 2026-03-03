2f769d427399d8058db12fcb864131bda11861a9
Elijah <elijahgharvey@gmail.com>
2026-03-03 12:36:33 +1100

    First commit, adding AdditionalApps

---
+++ AdditionalApps/JavaApp/app/src/main/java/com/example/javaapp/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.javaapp;
+
+import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
+
+import android.media.Image;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        //Handle Button
+        Button helloWorldButton = findViewById(R.id.helloWorldButton);
+        ImageView goku = findViewById(R.id.goku);
+        helloWorldButton.setOnClickListener(v ->{
+            Toast.makeText(this, "Clicked!!!", Toast.LENGTH_SHORT).show();
+            goku.setVisibility(VISIBLE);
+        });
+    }
+}
---
+++ AdditionalApps/README.md
@@ -0,0 +1,4 @@
+# Additional Apps
+These are a series of apps that I made either during workshops or in my own time to further my learning, test something I wasn't sure of or just to have a little bit of fun.
+
+* JavaApp is the app developed in the first weeks workshop (named poorly cause I created it before the workshop and kept accidentally making Kotlin apps)
